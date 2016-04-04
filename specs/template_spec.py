d360a936d3fac989a2c5879679632cb8f41b07c8
Eric Villard <dev@eviweb.fr>
2016-04-04 10:33:47 +0200

    add specification for README.md file content
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- specs/template_spec.py
+++ specs/template_spec.py
@@ -56,3 +56,16 @@ with description('Cookiecutter Template'):
             expect(actual).to(contain(config['default_context']['full_name']))
             expect(actual).to(contain(config['default_context']['email']))
             expect(actual).to(contain(datetime.now().strftime("%Y")))
+
+        with it('fills the README.md project name, project descritpion, project directory name and github username'):
+            config = get_config(SettingObject.CONFIG_FILE)
+            expected_description = 'My dummy project short descritpion'
+            self.settings.extra_context["project_short_description"] = expected_description
+            self.runner.run()
+            f = open(self.project_dir + '/README.md', 'r')
+            actual = f.read()
+
+            expect(actual).to(contain(DEFAULT_PROJECT))
+            expect(actual).to(contain(DEFAULT_PROJECT_DIR))
+            expect(actual).to(contain(expected_description))
+            expect(actual).to(contain(config['default_context']['github_username']))
