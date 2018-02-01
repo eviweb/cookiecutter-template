bc7b706657480b2fd93ad4010a50d962e07f2a20
Bruno Alla <browniebroke@users.noreply.github.com>
2018-02-01 19:16:43 +0000

    Add tests to the output template project (#6)
    * Add tests to the output template project
    
    * Fix dependencies so that build doesn't break

---
+++ {{cookiecutter.project_slug}}/tests/test_bake_project.py
@@ -0,0 +1,30 @@
+from contextlib import contextmanager
+
+import os
+
+
+@contextmanager
+def inside_dir(dirpath):
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+def test_project_tree(cookies):
+    result = cookies.bake(extra_context={'project_slug': 'test_project'})
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project.basename == 'test_project'
+
+
+def test_run_flake8(cookies):
+    result = cookies.bake(extra_context={'project_slug': 'flake8_compat'})
+    with inside_dir(str(result.project)):
+        assert os.subprocess.check_call(['flake8']) == 0
